f4f5ca7d77109c31a5a37d5095464e9808e03e0e
SetSM <sonomashi181@gmail.com>
2025-11-26 14:19:07 +0300

    first commit

---
+++ Java Nachalo/Zadanie lecture 1,2/Lecture1.js
@@ -0,0 +1,15 @@
+
+// /**
+// //  * 
+// //  * @param {any} name - Имена
+// //  * @param {number} age - Возраст
+// //  * @returns 
+// //  */
+function hello(name, age) {
+    return `Привет, ${name}! Твой возраст: ${age}}`;
+}
+
+console.log(hello("Николь", 35));
+console.log(hello("Данат", 50));
+console.log(hello("Даня", 22));
+
---
+++ Java Nachalo/Zadanie lecture 1,2/Lecture2.js
@@ -0,0 +1,57 @@
+//Задание 1
+
+// const Price = 599.99;
+// const Coast = 250;
+
+// const totalCost = ((Price) * 3 + 250).toFixed(2);
+
+// console.log(`Общая стоимость ${totalCost} `);
+
+// console.log(`Проверка ${typeof totalCost}`);  
+
+//Задание 2
+
+// const number1 = 42;
+// const number2 = 53;
+
+// const chetnoe = number1 % 2 === 0;
+// const nechetnoe = number2 % 2 === 0;
+
+// if ((number1) % 2 === 0) {
+//     console.log("Число 42 " + chetnoe)
+// }
+// else {
+//     console.log("Число 42 " + nechetnoe)
+// }
+
+// if ((number2) % 2 === 0) {
+//     console.log("Число 53 " + chetnoe)
+// }
+// else {
+//     console.log("Число 53 " + nechetnoe)
+// }
+
+// Задание 3
+
+// const ID = Math.floor(Math.random() * 100) + 1
+// if (1 <= (ID) <= 100) {
+//     console.log(ID)
+// }
+// else {
+//     console.log("Ошибка. Давай по новой")
+// }
+// Задание 4
+
+// let transactionId = 9999999999999999n;
+
+// let newId = transactionId + 1n;
+
+// let Dada = typeof newId;
+
+// console.log( Dada);
+
+// // Задание 5
+
+// let dialog = `Are you ready?\n${"yes!".toUpperCase()}`;
+// console.log(dialog);
+// console.log("Длина:", dialog.length);
